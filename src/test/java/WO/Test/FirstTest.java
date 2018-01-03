0a4c46a3f1091e290bca076b22caba6837a42781
rauts <Shyam.Raut@viacomcontractor.com>
2018-01-03 15:59:21 -0500

    debug

--- src/test/java/WO/Test/FirstTest.java
+++ src/test/java/WO/Test/FirstTest.java
@@ -9,12 +9,18 @@ public class FirstTest {
 	@Test
 	public void FirstTestNG() throws Exception{
 		
-		String command ="\"C:/Users/boset/AppData/Local/UiPath/UiStudio.exe"+"/file:"+"C:/Users/boset/Documents/UiPath/WON_Final_Scenarios/WONDeal_v1_maximize.xaml\" ";
+		String command ="\"C:/Users/boset/AppData/Local/UiPath/UiStudio.exe\" "+" \"/file:\" "+" \"C:/Users/boset/Documents/UiPath/WON_Final_Scenarios/WONDeal_v1_maximize.xaml\" ";
 		System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTTTTT" +command);
 		CommandExecutor.exeCmd(null, command);
 
 		System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
+	
 		
+		
+		
+		
+
+				
 	}
 	
 }
